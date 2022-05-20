5648f16985d6f068efa8c7051966f9832c15faf9
katsuhiro sato <katty@katty.local>
2022-05-20 14:10:52 +0900

    内容の削除

--- src/pracetice11/HumanInfo.java
+++ src/pracetice11/HumanInfo.java
@@ -10,15 +10,6 @@ public class HumanInfo {
 	
 	private String role;
 	
-	private int enScore;
-	
-	private int jaScore;
-	
-	private int mathScore;
-	
-	private int socialScore;
-	
-	private int scienceScore;
 	
 	public void setName(String firstName, String lastName) {
 		this.firstName = firstName;
@@ -35,42 +26,6 @@ public class HumanInfo {
 	
 	public int getAge() {
 		return age;
-	}
-	
-	public void setRole(String role) {
-		this.role = role;
-	}
-	
-	public String getRole() {
-		return role;
-	}
-	
-	public void setSubject(int enScore, int jaScore, int mathScore, int socialCore, int scienceScore) {
-		this.enScore = enScore;
-		this.jaScore = jaScore;
-		this.mathScore = mathScore;
-		this.socialScore = socialCore;
-		this.scienceScore = scienceScore;
-	}
-	
-	public int getEnScore() {
-		return enScore;
-	}
-	
-	public int getjaScore() {
-		return jaScore;
-	}
-	
-	public int getmathScore() {
-		return mathScore;
-	}
-	
-	public int getSocialScore() {
-		return socialScore;
-	}
-	
-	public int getScienceScore() {
-		return scienceScore;
 	}
 
 }
--- src/pracetice11/Main.java
+++ src/pracetice11/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		//TODO④: チーム宇治抹茶の役割を1次元配列で宣言してください。(配列を学ぼう) 
-		String[] role = {"役割1", "役割2", "役割3", "役割4", "役割5"};
 		
 		
 		//TODO①: インスタンス化 memberNameの箇所をチームメンバーの名前に変えてね
@@ -15,40 +14,25 @@ public class Main {
 		HumanInfo memberName4 = new HumanInfo();
 		HumanInfo memberName5 = new HumanInfo();
 		
-		Method method = new Method();
-		
 		
 		//TODO②: 各インスタンスに名前をセットしよう。
-		memberName1.setName("佐藤", "克洋");
-		memberName2.setName("牛島", "智也");
-		memberName3.setName("坂根", "未紗");
+		
 		
 		//TODO③: 各インスタンスに年齢をセットしよう。(かっちゃん：22歳、みかちゃん：22歳、みちゃ：23歳、こっちゃん：22歳、うっしー：22歳)
-		memberName1.setAge(22);
-		memberName2.setAge(22);
-		memberName3.setAge(23);
+		
 		
 		//TODO⑤: 各インスタンスにTODO③で作成した配列を使用して役割をセットしよう。
-		memberName1.setRole(role[0]);
-		memberName2.setRole(role[1]);
-		memberName3.setRole(role[2]);
+		
 		
 		//TODO⑥: Method.javaで各インスタンス情報を全て(名前、年齢、役割)表示するメソッドを呼び出して各インスタンスの情報をコンソール表示してね。
-		method.ShowHumanInfo(memberName1);
-		method.ShowHumanInfo(memberName2);
-		method.ShowHumanInfo(memberName3);
+		
 		
 		//TODO⑦（こっからは抽象的に指示するから必要な処理を頑張って描いてね）
 		// Method.javaに渡した2人の合計点数比較できるようにする。点数が高い方の名前を表示する。
-		//（例）インスタンス1：国語30点　と　インスタンス2：国語70点の比較 → 〇〇さんの方が点数が高い
-		memberName1.setSubject(100, 200, 30, 40, 50);
-		memberName2.setSubject(30, 40, 50, 60, 70);
-		memberName3.setSubject(400, 50, 60, 70, 80);
-		method.ShowResultScore(memberName1, memberName2);
 		
-		//TODO⑧ Method.javaにTODO⑦で作ったメソッド名と同じ名前でインスタンス同士の2つの科目合計を比較して、点数が高い方の名前を表示してね。
-		//（例）インスタンス1：80点(国語：30点, 英語：50点)　と　インスタンス2：120点(国語：70点, 英語：50点)の比較 → 〇〇さんの方が点数が高い
-		method.ShowResultScore(memberName1, memberName2, memberName3);
+		
+		//TODO⑧ Method.javaにTODO⑦で作ったメソッド名と同じ名前で3人の合計点数比較できるようにする。点数が高い人の名前を表示する。
+
 	}
 
 }
